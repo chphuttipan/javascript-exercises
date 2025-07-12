c59c97861b7fbcca6d07ff03d9b457477b94c3d5
chphuttipan <phuttipan.ch@gmail.com>
2025-07-12 21:57:32 +0700

    Create a calculator function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total = array.reduce((num1, num2) => num1 + num2, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((num1, num2) => num1 * num2, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	const total = Math.pow(num1, num2);
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let num = 1;
+  for (let i = a; i > 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
