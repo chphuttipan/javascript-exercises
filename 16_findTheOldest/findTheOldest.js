c73d3ee50b3dfb45de3f70380f4d5dc19219a987
chphuttipan <phuttipan.ch@gmail.com>
2025-07-13 13:52:25 +0700

    findTheOldest checked

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.forEach(person => {
+        if (person.yearOfBirth) {
+            if (person.yearOfDeath) {
+                person.age = person.yearOfDeath - person.yearOfBirth;
+            }
+            else {
+                const currentYear = new Date().getFullYear();
+                person.age = currentYear - person.yearOfBirth;
+            }
+        }
+        
+        else {
+            person.age = null;
+        }
+    })
 
+    const elderPeople = [...arr].sort((a, b) => b.age - a.age);
+    
+    console.log(elderPeople);
+    return elderPeople.at(0);
 };
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
