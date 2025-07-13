9aa8fb399cb908d63e083757b691225661540682
chphuttipan <phuttipan.ch@gmail.com>
2025-07-13 07:21:00 +0700

    Palindromes checked

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // Consider letters and numbers
+    alphabetnumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    
+    // Clean the word
+    const cleanWord = word
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphabetnumerical.includes(character))
+        .join('');
+    
+    // Check word.length is odd or even
+    const reverseWord = cleanWord.split('').reverse().join('');
 
+    if (cleanWord === reverseWord) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
